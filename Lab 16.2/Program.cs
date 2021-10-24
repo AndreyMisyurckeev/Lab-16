edbf9bf4455bbc3eaf09eeed4f3098652ebd4375
AndreyMisyurckeev <91983861+AndreyMisyurckeev@users.noreply.github.com>
2021-10-24 19:58:21 +0300

    Добавьте файлы проекта.

---
+++ Lab 16.2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace Lab_16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int n = 5;
+            string[] jsonString = new string[n];
+            string path = @"C:\Users\misyu\OneDrive\C#\Задания\16\Lab 16\Lab 16\bin\Debug\Products.json";
+            double maxPrice = 0;
+            string maxName = "";
+            Product[] product = new Product[n];
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    jsonString[i] = streamReader.ReadLine();
+                    product [i] = JsonSerializer.Deserialize<Product>(jsonString[i]);
+                }
+            }
+            foreach (Product a in product)
+            {
+                if (maxPrice < a.Price)
+                {
+                    maxPrice = a.Price;
+                    maxName = a.Name;
+                }
+            }
+            Console.WriteLine($"Самый дорогой товар - {maxName}");
+            Console.ReadKey();
+        }
+    }
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Lab 16/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace Lab_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int n = 5;
+            string[] jsonString = new string[n];
+            string path = "Products.json";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            Product[] product = new Product[n];
+            for (int i = 0; i < n; i++)
+            {
+                product[i] = new Product();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите код товара - ");
+                product[i].Code = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите наименование товара - ");
+                product[i].Name = Console.ReadLine();
+                Console.Write("Введите стоимость товара - ");
+                product[i].Price = Convert.ToDouble(Console.ReadLine());
+            }
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    string jsonstring = JsonSerializer.Serialize(product[i]);
+                    sw.WriteLine(jsonstring);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
